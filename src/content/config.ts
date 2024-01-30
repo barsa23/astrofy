8941ecf77b140f9108d7d801133442396528d3ab
hule <huleweb@gmail.com>
2024-01-30 15:00:54 -0300

    aver

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,12 @@ const blogSchema = z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    updatedDate: z.string().optional(),
     heroImage: z.string().optional(),
-    badge: z.string().optional(),
-    tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
-        message: 'tags must be unique',
-    }).optional(),
+    // updatedDate: z.string().optional(),
+    // badge: z.string().optional(),
+    // tags: z.array(z.string()).refine(items => new Set(items).size === items.length, {
+    //     message: 'tags must be unique',
+    // }).optional(),
 });
 
 const storeSchema = z.object({
